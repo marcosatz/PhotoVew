e54c622c8351225027cb56ef610e16a530dcc13d
unknown <ryanjhunter00@gmail.com>
2020-05-05 20:52:22 -0600

    first commit 2

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -10,21 +10,52 @@ $(document).ready(function(){
     });
 
     // ============================ MODAL ==============================//
-    $('.icon-play').click(function(e) {
-        e.preventDefault();
+    // $('.icon-play').click(function(e) {
+    //     e.preventDefault();
+    //     $('.icon-play').parent();
+    //     $('.modal-video').fadeToggle();
+    //     $('body').css({scroll: none})
+    // });
+
+    $('.modal-video').click(function() {
         $('.modal-video').fadeToggle();
-        $('body').css({scroll: none})
+        $('#yt-video-id').attr('src', '');
     });
 
-    $('.close-modal').click(function() {
-        $('.modal-video').fadeToggle();
+    $('.modal-container').click(function(e) {
+        e.stopPropagation();
     });
 
-     // ============================ Gallery ==============================//
+    function yt_get_video_id(url) {
+		var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+		var match = url.match(regExp);
+		return (match&&match[7].length==11)? match[7] : false;
+    };
+
+	$('.video').on('click', function() {
+		var url = $(this).attr('id');
+        var id = yt_get_video_id(url);
+        
+
+        $('.modal-video').fadeToggle();
+		// $('#yt-video-id').val(id);
+        $('#yt-video-id').attr('src', 'https://www.youtube.com/embed/' + id + '?autoplay=1');
+        
+    });
+    
+    function modalHeight() {
+        var modalHeight = $('.modal-video').height();
+        $('#yt-video-id').css('height', modalHeight + 'px');
+    }
+
+
+    // ============================ Gallery ==============================//
     lightbox.option({
         'resizeDuration': 200,
         'wrapAround': true,
         'alwaysShowOnTouchDevices': true,
         'disableScrolling': true,
     });
+
+    $('.hero-slide').slick();
 });
